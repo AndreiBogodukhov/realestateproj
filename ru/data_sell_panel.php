661f345c6e5a1cb06d49075d2e20f5982e681e80
Andrei Bogodukhov <a.bogodukhov.98@gmail.com>
2024-01-30 20:16:24 +0500

    Update data_sell_panel.php

--- ru/data_sell_panel.php
+++ ru/data_sell_panel.php
@@ -47,10 +47,29 @@ if (!isset($_SESSION['admin_id'])) {
                             <tbody>
                                 <tr>
                                     <td class="table-plus">Квартира NanTao</td>
-                                    <td>NanTan</td>
-                                    <td>Sagittarius</td>
+                                    <td>200 000</td>
+                                    <td>Nan Tao</td>
                                     <td>Trainer Avenue Peoria</td>
                                     <td>2018</td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                                                <i class="dw dw-more"></i>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                                                <a class="dropdown-item" href="#"><i class="dw dw-eye"></i> View</a>
+                                                <a class="dropdown-item" href="#"><i class="dw dw-edit2"></i> Edit</a>
+                                                <a class="dropdown-item" href="#"><i class="dw dw-delete-3"></i> Delete</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="table-plus">Квартира 2</td>
+                                    <td>400 000</td>
+                                    <td>Bang Tao</td>
+                                    <td>Nissa</td>
+                                    <td>2020</td>
                                     <td>
                                         <div class="dropdown">
                                             <a class="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
